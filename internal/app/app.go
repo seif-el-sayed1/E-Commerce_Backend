9089457255f8ee9b056f6877eeaa053934f2baf3
seif_elsayed <seifsayed121@gmail.com>
2026-08-10 01:55:36 +0300

    change connect to db package name to database

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,16 +3,17 @@ package app
 import (
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/admin"
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/config"
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/database"
 )
 
 /**
- Package initialization entry point.
+ this Package initialization entry point.
  This file aggregates initialization functions from different packages
  across the application and executes them when the server starts.
 **/
 
 func Run() {
 	config.LoadEnv()
-	config.ConnectToDb()
-	admin.SeedSuperAdmin(config.DB, config.Env.SuperAdminEmail, config.Env.SuperAdminPassword)
+	database.ConnectToDb()
+	admin.SeedSuperAdmin(database.DB)
 }
