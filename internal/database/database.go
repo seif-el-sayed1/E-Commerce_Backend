78b9ac5f7979662da3343efc1d2003fe7cb55dbd
seif_elsayed <seifsayed121@gmail.com>
2026-08-09 19:57:23 +0300

    move the database file to aviod circular import

---
+++ internal/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/admin"
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/constants"
+)
+
+var DB *gorm.DB
+
+func ConnectToDb() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(constants.Error("❌ Failed to connect to database: "), err)
+	}
+
+	log.Println(constants.Success("🚀 Database connected successfully"))
+	DB = db
+
+	MigrateModels()
+}
+
+func MigrateModels() {
+	err := DB.AutoMigrate(
+		admin.Admin{},
+	)
+	if err != nil {
+		log.Fatal(constants.Error("Failed to migrate database: "), err)
+	}
+
+	log.Println(constants.Success(" 🚀 Database migrated successfully"))
+}
