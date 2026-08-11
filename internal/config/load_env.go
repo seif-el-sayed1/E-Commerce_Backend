b0a76c6e0c0425f3d2cf96e4c07ad3ed34d14f97
seif_elsayed <seifsayed121@gmail.com>
2026-08-11 23:45:52 +0300

    refactor: enhance LoadEnv function to include SuperAdmin fields in configuration

--- internal/config/load_env.go
+++ internal/config/load_env.go
@@ -9,6 +9,19 @@ import (
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/constants"
 )
 
+type Config struct {
+	DatabaseURL         string
+	Port                string
+	JWTSecret           string
+	JWTExpiration       string
+	SuperAdminEmail     string
+	SuperAdminPassword  string
+	SuperAdminFirstName string
+	SuperAdminLastName  string
+}
+
+var Env Config
+
 func LoadEnv() {
 	if err := godotenv.Load(); err != nil {
 		log.Println(constants.Warning("⚠️ No .env file found, reading from system environment variables"))
@@ -25,6 +38,17 @@ func LoadEnv() {
 		if os.Getenv(key) == "" {
 			log.Fatal(constants.Error("❌ Missing required environment variable: "), key)
 		}
+	}
+
+	Env = Config{
+		DatabaseURL:         os.Getenv("DATABASE_URL"),
+		Port:                os.Getenv("PORT"),
+		JWTSecret:           os.Getenv("JWT_SECRET"),
+		JWTExpiration:       os.Getenv("JWT_EXPIRATION"),
+		SuperAdminEmail:     os.Getenv("SUPER_ADMIN_EMAIL"),
+		SuperAdminPassword:  os.Getenv("SUPER_ADMIN_PASSWORD"),
+		SuperAdminFirstName: os.Getenv("SUPER_ADMIN_FIRST_NAME"),
+		SuperAdminLastName:  os.Getenv("SUPER_ADMIN_LAST_NAME"),
 	}
 
 	log.Println(constants.Success("✅ Environment variables loaded"))
