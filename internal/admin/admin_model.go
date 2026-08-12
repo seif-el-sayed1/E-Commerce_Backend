261b45a7a80d8a9f61e9d14f76782dd63f5cdb1f
seif_elsayed <seifsayed121@gmail.com>
2026-08-12 22:57:40 +0300

    feat: implement GenerateToken method for Admin struct to create JWT tokens

--- internal/admin/admin_model.go
+++ internal/admin/admin_model.go
@@ -1,8 +1,13 @@
 package admin
 
 import (
+	"strconv"
+	"strings"
 	"time"
 
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/config"
+
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -19,6 +24,7 @@ type Admin struct {
 	IsVerified   bool `json:"is_verified" gorm:"default:false"`
 	IsBlocked    bool `json:"is_blocked" gorm:"default:false"`
 	IsDeleted    bool `json:"is_deleted" gorm:"default:false"`
+	IsActive     bool `json:"is_active" gorm:"default:true"`
 
 	Password               string     `json:"password" gorm:"not null"`
 	PasswordChangedAt      *time.Time `json:"password_changed_at"`
@@ -35,9 +41,31 @@ type Admin struct {
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
-func (a *Admin) BeforeCreate(tx *gorm.DB) (err error) {
-	if a.ID == uuid.Nil {
-		a.ID = uuid.New()
+func (a *Admin) GenerateToken(db *gorm.DB) (string, time.Time, error) {
+	expStr := config.Env.JWTExpiration
+	days, err := strconv.Atoi(strings.TrimSuffix(expStr, "d"))
+	if err != nil {
+		return "", time.Time{}, err
 	}
-	return
+	tokenExpDate := time.Now().AddDate(0, 0, days)
+
+	claims := jwt.MapClaims{
+		"userId": a.ID.String(),
+		"role":   a.Role,
+		"exp":    tokenExpDate.Unix(),
+	}
+
+	tokenObj := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenObj.SignedString([]byte(config.Env.JWTSecret))
+	if err != nil {
+		return "", time.Time{}, err
+	}
+
+	a.Token = &token
+	a.TokenExpDate = &tokenExpDate
+	if err := db.Save(a).Error; err != nil {
+		return "", time.Time{}, err
+	}
+
+	return token, tokenExpDate, nil
 }
