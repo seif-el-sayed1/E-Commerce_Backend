fa8e8f851bc309e860b8bed72d20445b14d15ae3
seif_elsayed <seifsayed121@gmail.com>
2026-08-15 01:56:37 +0300

    feat: update Run function to accept server parameter and integrate middleware

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,10 @@ import (
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/admin"
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/config"
 	"seif-el-sayed1/E-Commerce_Backend.git/internal/database"
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/middlewares"
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 /**
@@ -12,8 +16,13 @@ import (
  across the application and executes them when the server starts.
 **/
 
-func Run() {
+func Run(server *gin.Engine) {
 	config.LoadEnv()
 	database.ConnectToDb()
 	admin.SeedSuperAdmin(database.DB)
+
+	// Middlewares
+	server.Use(middlewares.GlobalError())
+
+	routes.AppRoutes(server, database.DB)
 }
