301859724f7b10a4f81dc8f956c0136b80c16d1b
seif_elsayed <seifsayed121@gmail.com>
2026-08-18 00:16:38 +0300

    add admin change password route

--- internal/admin/admin_auth_routes.go
+++ internal/admin/admin_auth_routes.go
@@ -5,11 +5,14 @@ import (
 	"gorm.io/gorm"
 )
 
-func AdminAuthRoutes(rg *gin.RouterGroup, db *gorm.DB, protect gin.HandlerFunc) {
+func AdminAuthRoutes(rg *gin.RouterGroup, db *gorm.DB, protect gin.HandlerFunc, allowedTo func(roles ...string) gin.HandlerFunc) {
 	auth := rg.Group("/admins/auth")
 	{
 		auth.POST("/login", func(c *gin.Context) {
 			Login(c, db)
+		})
+		auth.PATCH("/change-password", protect, func(c *gin.Context) {
+			AdminChangePassowrd(c, db)
 		})
 	}
 
