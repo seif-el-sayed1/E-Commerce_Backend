92aa6090bfcb2dafd473ded68bda54fab1637b7d
seif_elsayed <seifsayed121@gmail.com>
2026-08-19 20:25:53 +0300

    feat: implement AddAdmin function to create new admin and send verification email

--- internal/admin/admin_controller.go
+++ internal/admin/admin_controller.go
@@ -1,33 +1,51 @@
 package admin
 
 import (
-	"errors"
 	"net/http"
-	"seif-el-sayed1/E-Commerce_Backend.git/internal/utils"
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/email"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func GetMyProfile(c *gin.Context, db *gorm.DB) {
+// @desc    Add new admin
+// @route   POST /admins/add-admin
+// @access  Private
+func AddAdmin(c *gin.Context, db *gorm.DB) {
+	var body Add
+	if !ValidateAddAdmin(c, &body) {
+		return
+	}
 	var admin Admin
+	admin.FirstName = body.FirstName
+	admin.LastName = body.LastName
+	admin.Email = body.Email
+	admin.Phone = body.Phone
 
-	adminId := utils.GetUserID(c)
-
-	result := db.First(&admin, "id=?", adminId)
+	result := db.Create(&admin)
 	if result.Error != nil {
-		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			c.Error(utils.NewApiError("Profile not found", http.StatusNotFound))
-		} else {
-			c.Error(result.Error)
-		}
+		c.Error(result.Error)
+		c.Abort()
+		return
+	}
+
+	token, _, err := admin.GenerateToken(db)
+	if err != nil {
+		c.Error(err)
+		c.Abort()
+		return
+	}
+
+	err = email.AdminVerificationEmail(token, body.Email)
+	if err != nil {
+		c.Error(err)
 		c.Abort()
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
-		"message": "Profile fetched successfully",
+		"message": "Admin added successfully",
 		"data": gin.H{
 			"id":             admin.ID,
 			"first_name":     admin.FirstName,
@@ -40,4 +58,5 @@ func GetMyProfile(c *gin.Context, db *gorm.DB) {
 			"is_verified":    admin.IsVerified,
 		},
 	})
+
 }
