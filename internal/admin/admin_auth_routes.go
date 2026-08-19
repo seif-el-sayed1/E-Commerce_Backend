fc01556218a80fd1504d6ab8d9564ef5367f5504
seif_elsayed <seifsayed121@gmail.com>
2026-08-20 01:24:38 +0300

    fix: correct typo in AdminChangePassword function call

--- internal/admin/admin_auth_routes.go
+++ internal/admin/admin_auth_routes.go
@@ -1,6 +1,8 @@
 package admin
 
 import (
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/constants"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -12,7 +14,16 @@ func AdminAuthRoutes(rg *gin.RouterGroup, db *gorm.DB, protect gin.HandlerFunc, 
 			Login(c, db)
 		})
 		auth.PATCH("/change-password", protect, func(c *gin.Context) {
-			AdminChangePassowrd(c, db)
+			AdminChangePassword(c, db)
+		})
+		auth.POST("/verify-account", protect, allowedTo(constants.Roles.SuperAdmin, constants.Roles.Admin), func(c *gin.Context) {
+			VerifyAdminAccount(c, db)
+		})
+		auth.POST("forget-password", func(c *gin.Context) {
+			AdminForgetPassword(c, db)
+		})
+		auth.PATCH("/reset-password/:token", func(c *gin.Context) {
+			AdminResetPassword(c, db)
 		})
 	}
 
