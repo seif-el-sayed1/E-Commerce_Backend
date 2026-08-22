6c154e6259de5ed1b3d0433b00c2f908f52014d9
seif_elsayed <seifsayed121@gmail.com>
2026-08-22 23:29:22 +0300

    feat: add admin management routes for adding, fetching, updating, and deleting admins

--- internal/admin/admin_routes.go
+++ internal/admin/admin_routes.go
@@ -13,6 +13,21 @@ func AdminRoutes(rg *gin.RouterGroup, db *gorm.DB, protect gin.HandlerFunc, allo
 		adminRoutes.GET("/profile", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
 			GetMyProfile(c, db)
 		})
-	}
+		adminRoutes.POST("/add-admin", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
+			AddAdmin(c, db)
+		})
+		adminRoutes.GET("/", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
+			GetAllAdmins(c, db)
+		})
+		adminRoutes.GET("/:id", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
+			GetSingleAdmin(c, db)
+		})
+		adminRoutes.PATCH("/:id", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
+			UpdateAdmin(c, db)
+		})
+		adminRoutes.DELETE("/:id", protect, allowedTo(constants.Roles.SuperAdmin), func(c *gin.Context) {
+			DeleteAdmin(c, db)
+		})
 
+	}
 }
