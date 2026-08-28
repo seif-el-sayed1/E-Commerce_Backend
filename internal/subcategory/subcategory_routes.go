6116338c455bda935691a4ae5ae0d84e9c504ece
seif_elsayed <seifsayed121@gmail.com>
2026-08-28 16:29:41 +0300

    subcategory routes

---
+++ internal/subcategory/subcategory_routes.go
@@ -0,0 +1,31 @@
+package subcategory
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"seif-el-sayed1/E-Commerce_Backend.git/internal/constants"
+)
+
+func SubCategoryRoutes(rg *gin.RouterGroup, db *gorm.DB, protect gin.HandlerFunc, allowedTo func(roles ...string) gin.HandlerFunc) {
+	subcats := rg.Group("/subcategories")
+	{
+		// Public
+		subcats.GET("/", func(c *gin.Context) {
+			GetSubCategories(c, db)
+		})
+		subcats.GET("/:id", func(c *gin.Context) {
+			GetSubCategory(c, db)
+		})
+
+		subcats.POST("/", protect, allowedTo(constants.Roles.Admin, constants.Roles.SuperAdmin), func(c *gin.Context) {
+			CreateSubCategory(c, db)
+		})
+		subcats.PUT("/:id", protect, allowedTo(constants.Roles.Admin, constants.Roles.SuperAdmin), func(c *gin.Context) {
+			UpdateSubCategory(c, db)
+		})
+		subcats.DELETE("/:id", protect, allowedTo(constants.Roles.Admin, constants.Roles.SuperAdmin), func(c *gin.Context) {
+			DeleteSubCategory(c, db)
+		})
+	}
+}
