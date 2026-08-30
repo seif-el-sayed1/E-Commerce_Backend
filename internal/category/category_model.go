46a58498942b44580b391086fb4d009c5d0f4f0a
seif_elsayed <seifsayed121@gmail.com>
2026-08-31 01:56:49 +0300

    refactor: reorganize imports and enhance Category struct with timestamps

--- internal/category/category_model.go
+++ internal/category/category_model.go
@@ -1,9 +1,15 @@
 package category
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Category struct {
-	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
-	Name    string    `json:"name" gorm:"not null;unique;index:category_name"`
-	CatType string    `json:"category_type" gorm:"not null;default:'product';check:type IN ('women','men')"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
+	Name      string    `json:"name" gorm:"not null;unique;index:category_name"`
+	CatType   string    `json:"category_type" gorm:"not null;default:'product';check:cat_type IN ('product','women','men')"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
